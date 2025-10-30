424ad321112ff82c0cd67aa895a90ca2f6a15985
mikuloveSSY <1323342419@qq.com>
2025-10-30 19:06:24 +0800

    extract TurnRight()

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -25,15 +25,7 @@ void ExecutorImpl::Execute(const std::string &commands) noexcept {
     } else if (cmd == 'L') {
       TurnLeft();
     } else if (cmd == 'R') {
-      if (pose.heading == 'E') {
-        pose.heading = 'S';
-      } else if (pose.heading == 'W') {
-        pose.heading = 'N';
-      } else if (pose.heading == 'N') {
-        pose.heading = 'E';
-      } else if (pose.heading == 'S') {
-        pose.heading = 'W';
-      }
+      TurnRight();
     } else if (cmd == 'F') {
       isFast = !isFast;
     }
@@ -60,6 +52,17 @@ void ExecutorImpl::TurnLeft() noexcept {
     pose.heading = 'W';
   } else if (pose.heading == 'S') {
     pose.heading = 'E';
+  }
+}
+void ExecutorImpl::TurnRight() noexcept {
+  if (pose.heading == 'E') {
+    pose.heading = 'S';
+  } else if (pose.heading == 'W') {
+    pose.heading = 'N';
+  } else if (pose.heading == 'N') {
+    pose.heading = 'E';
+  } else if (pose.heading == 'S') {
+    pose.heading = 'W';
   }
 }
 } // namespace adas
