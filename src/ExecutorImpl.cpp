074cce9a734bdd38941d90854fe9ce122c4d36cc
mikuloveSSY <1323342419@qq.com>
2025-10-30 19:32:28 +0800

    extract TurnRightCommand

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -32,7 +32,9 @@ void ExecutorImpl::Execute(const std::string &commands) noexcept {
           std::make_unique<TurnLeftCommand>();
       cmder->DoOperate(*this);
     } else if (cmd == 'R') {
-      TurnRight();
+      std::unique_ptr<TurnRightCommand> cmder =
+          std::make_unique<TurnRightCommand>();
+      cmder->DoOperate(*this);
     } else if (cmd == 'F') {
       isFast = !isFast;
     }
